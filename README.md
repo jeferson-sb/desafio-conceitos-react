d18c5f6e91018665c0fd77dd8ad50bd3917998a4
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2020-06-20 14:24:03 -0300

    Complete Challenge ✅

---
+++ README.md
@@ -0,0 +1,5 @@
+# Desafio de Conceitos React
+
+Desafio de conceitos react do Bootcamp GoStack
+
+Regras do desafio: [https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-reactjs](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-reactjs)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-import "./styles.css";
+import './styles.css';
+import api from './services/api';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then((response) => {
+      setRepositories(response.data);
+    });
+  });
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      url: 'https://github.com/facebook/react',
+      title: 'React',
+      techs: ['React', 'JavaScript'],
+    });
+
+    const newRepository = response.data;
+    setRepositories([...repositories, newRepository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    const remainingRepos = repositories.filter((repo) => repo.id === id);
+    setRepositories(remainingRepos);
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <ul data-testid='repository-list'>
+        {repositories.map((repo) => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
